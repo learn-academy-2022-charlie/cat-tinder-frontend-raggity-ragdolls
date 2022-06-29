1adc3d77dc6a34671bc85b077c64a391df5ee837
SOMarrah <stephenomarrah@gmail.com>
2022-06-29 08:32:28 -0700

    finished header and footer with animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,16 @@ class Header extends Component {
   render() {
     return (
     <>
-        <div class="blue-bg"></div>
-        <div class="white-bg shadow"></div>
-        <div class="content">
-            <h1>Slanted & Enchanted</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin magna ex, elementum id convallis eu, elementum id justo. Maecenas dictum sagittis leo, ut ultrices risus mollis vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-                eu lectus felis. Vivamus eget velit erat. Vivamus condimentum augue sed lacus viverra imperdiet. Etiam quis lectus non libero hendrerit porta. In sit amet molestie lacus. Praesent ultricies, lectus eget porta porttitor, sem sapien facilisis arcu,
-                eu tincidunt nulla odio ac ipsu.</p>
-
-            <p>Praesent eu arcu convallis, faucibus turpis eu, rutrum nisi. Integer rutrum ipsum at aliquam consequat. In dignissim lorem nibh, nec iaculis est fermentum vitae. Maecenas sodales, nunc eget lacinia volutpat, nibh mi lobortis leo, id varius urna tellus
-                ut magna. Sed porttitor nunc et luctus efficitur. Nam sit amet congue nulla. Praesent dapibus erat arcu, at scelerisque arcu faucibus vel. Morbi blandit venenatis elit. Nam feugiat bibendum suscipit. Donec viverra eu lorem eu venenatis. Nullam posuere
-                nulla id libero rutrum venenatis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam molestie ac velit vel blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-                egestas.</p>
-        </div>
+    <div id="headerContainer">
+        <header>
+            <h1>
+                Slasher™️
+            </h1>
+            <h2>
+                Get your scare on
+            </h2>
+        </header>
+      </div>
     </>
     )
   }
