a49189560dd353c933e68a8970e4829bfdc3a6fd
SOMarrah <stephenomarrah@gmail.com>
2022-07-05 07:04:08 -0700

    changed some more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,27 +26,24 @@ class Header extends Component {
     <div id="headerContainer">
         <header>
           <NavLink to='/'>
-            <h1>
-                Slasher™️
+            <h1 id="headerTitle">
+                Slasher
             </h1>
             </NavLink >
-            <h5>
-                Get your scare on
-            </h5>
             <div id="dropdownContainer">
             <Dropdown direction="down" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                <DropdownToggle caret>
-                  Dropdown
+                <DropdownToggle id='dropdownBtn' caret>
+                  Get your scare on
                 </DropdownToggle>
                 <DropdownMenu container="body">
                   <DropdownItem>
-                  <NavLink  to='/monsterindex'>
-                  <p>All Monsters</p>
+                  <NavLink className='navLinkText' to='/monsterindex'>
+                  <p >All Monsters</p>
                   </NavLink>
                   </DropdownItem>
                   <DropdownItem>
-                  <NavLink to='/monsternew'>
-                  <p>Make a New Monster</p>
+                  <NavLink className='navLinkText' to='/monsternew'>
+                  <p >Make a New Monster</p>
                   </NavLink>
                   </DropdownItem>
                 </DropdownMenu>
